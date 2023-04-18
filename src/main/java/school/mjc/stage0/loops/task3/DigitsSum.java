b1998e2c644c3fdbf04d202a43cf55dda5bcdf8b
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-18 16:06:47 +0500

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        if(t<0){
+            t*=-1;
+        }
+        int sum=0;
+        int res=0;
+        for(int i=t;i>0;i/=10){
+            res = i%10;
+            sum+=res;
+        }
+        System.out.println(sum);
 
     }
 }
