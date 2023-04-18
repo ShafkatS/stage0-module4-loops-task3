b55967b05296b7e967f0d4f01c5830336029f763
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-18 16:04:02 +0500

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+           int numberOne = 1;
+           int numberTwo = 1;
+           int numberThree = 0;
+           int getSumNumbersOneAndTwo;
+         System.out.print(numberThree + " " + numberOne + " " + numberTwo + " ");
+        for (int i = 4; i <= lastFibonacci; i++) {
+            getSumNumbersOneAndTwo = numberOne + numberTwo;
+            System.out.print(getSumNumbersOneAndTwo + " ");
+            numberOne = numberTwo;
+            numberTwo = getSumNumbersOneAndTwo;
     }
 }
