67677364dbcb184811e4d8c68ab1ec8c3da924b0
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-18 16:02:12 +0500

    Update GreatestCommonDivisor.java

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        if (first == 0 || second == 0) {
+            System.out.println(second);
+        } else if (first < second) {
+            temporaryVariable = first;
+            first = second;
+            second = temporaryVariable;
+            printGCD(first % second, second);
+        } else if (first % second == 0) {
+            System.out.println(second);
+        } else {
+            printGCD(second, first % second);
+        }
     }
 }
